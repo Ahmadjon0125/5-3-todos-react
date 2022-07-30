6e08fc62a879ffd2aad26198816d14efa7fc0830
Ahmadjon0125 <Ahmadjon0125@gmail.com>
2022-07-30 22:45:52 +0500

    5-3-todos-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react"
+import "./app.scss"
+import {List} from "./components"
+import {ListItem} from "./components"
+
 
 function App() {
+	const elInput = useRef()
+	
+	const [todos, setTodos] = useState(JSON.parse(window.localStorage.getItem("todos")) || [])
+	let all = todos.length
+	let complated = todos.filter(el => el.isComplated === true).length
+	let unComplated = todos.filter(el => el.isComplated === false).length
+	console.log(unComplated);
+	
+	const handleInputValu = (evt) =>{
+
+		evt.preventDefault()
+		const newTodo  = {
+					id: todos.at(-1)?.id ? todos.at(-1).id + 1 : 1,
+					text: elInput.current.value,
+					isComplated: false
+				}
+				setTodos([...todos,newTodo]);
+				elInput.current.value = null
+			
+	}
+
+	window.localStorage.setItem("todos", JSON.stringify(todos))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<form className="text-center my-5 row d-flex justify-content-center" onSubmit={handleInputValu}>
+				<div className="col-3"><input className="me-5 form-control " ref={elInput} type="text" placeholder="Todo..." /></div>
+				<button className="btn btn-success col-1" type="submit">Send</button>
+			</form>
+			{todos.length > 0 && <List>
+				{
+					todos.map(e => (
+						<ListItem key={e.id} item={e} todos= {todos} setTodos= {setTodos} />
+					))
+				}
+			</List>}
+			<div className="text-center">
+				<button className="btn btn-secondary">All:  <strong>  {all}</strong> </button>
+				<button className="mx-3 btn btn-primary">Complated:  <strong>  {complated}</strong> </button>
+				<button className="btn btn-info">Uncomplated:  <strong>  {unComplated}</strong> </button>
+			</div>
     </div>
   );
 }
